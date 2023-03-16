39d80f27f8fc923ae94460ba56153002cb36f81b
mesh-dell <mwaimonicas@gmail.com>
2023-03-16 23:36:48 +0300

    Functionality 1

--- app/src/main/java/com/example/chatapp/login.kt
+++ app/src/main/java/com/example/chatapp/login.kt
@@ -1,11 +1,28 @@
 package com.example.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
+private lateinit var edtEmail: EditText
+private lateinit var edtPassword: EditText
+private lateinit var btnLogin: Button
+private lateinit var btnSignup: Button
 class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignup = findViewById(R.id.btnSignUp)
+
+        btnSignup.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
     }
 }
